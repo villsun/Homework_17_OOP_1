8baa618c0a60b37b0848ca08b0942632c1da3f65
villsun <lonukkola@gmail.com>
2024-05-30 13:12:26 +0200

    Add function Show

--- Lesson_17(homework_1)/Homework.cpp
+++ Lesson_17(homework_1)/Homework.cpp
@@ -1,10 +1,28 @@
 #include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
+bool Sort_by_Letter(const std::string& str_1, const std::string& str_2)
+{
+	return str_1 < str_2;
+}
+
+void Show(vector<string> const obj)
+{
+	for (auto& element : obj)
+	{
+		cout << element << ' ';
+	}
+}
+
 int main()
 {
-
+	vector<string> obj{ "Asd", "Adr", "Ioa", "A", "Uoa", "Ta"};
+	sort(obj.begin(), obj.end(), Sort_by_Letter);
+	Show(obj);
 
 	return 0;
 }
